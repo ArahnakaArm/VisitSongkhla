9698237a09fc20761b089d42e2c85079c6c47ff7
Visitpattani <visitpattaniapp@gmail.com>
2019-03-09 03:01:58 +0700

    update menu page,add checkin page

--- app/src/main/java/com/example/deimos/visitsongkhla/Menu.java
+++ app/src/main/java/com/example/deimos/visitsongkhla/Menu.java
@@ -4,16 +4,29 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class Menu extends AppCompatActivity {
+    ViewFlipper viewFlipper;
     private static final int ACTIVITY_NUM=1;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
         setNavi();
+        int images[]={R.drawable.head2,R.drawable.navel,R.drawable.tail2};
+        viewFlipper = findViewById(R.id.viewflipper);
+
+       /* for(int i=0;i<images.length;i++){
+            flipperimage(images[i]);
+        }*/
+        for(int image:images){
+            flipperimage(image);
+        }
+
     }
     public void setNavi(){
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
@@ -23,5 +36,17 @@ public class Menu extends AppCompatActivity {
 
         MenuItem menuItem=menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+    public void flipperimage(int images){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(images);
+
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(3500);
+        viewFlipper.setAutoStart(true);
+
+        viewFlipper.setInAnimation(this,android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(this,android.R.anim.slide_out_right);
+
     }
 }
